70c06cc61f38ac5962748649aa3984fe5aaed437
cheddarChoi <cde1103@kaist.ac.kr>
2025-11-28 14:43:51 +0900

    Update CV

--- src/components/footer.js
+++ src/components/footer.js
@@ -19,7 +19,7 @@ const Footer = (props) => {
             </a>
           </div>
           <div className="col-4 text-end">
-            <div>Last Updated: Oct 4, 2025</div>
+            <div>Last Updated: Nov 28, 2025</div>
           </div>
         </div>
       </div>
